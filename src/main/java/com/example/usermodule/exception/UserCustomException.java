11c291cece7322eff1c73a623c263b719fc0c0a7
varshap-17 <varshap630@gmail.com>
2022-11-14 17:40:15 +0530

    user exception class

---
+++ src/main/java/com/example/usermodule/exception/UserCustomException.java
@@ -0,0 +1,7 @@
+package com.example.usermodule.exception;
+
+public class UserCustomException extends RuntimeException{
+    public UserCustomException(String message){
+        super(message);
+    }
+}
